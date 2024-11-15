07dea63b38544d26e5c094682fde160dd11ea80b
LuisCamachoRivero <camachoriveroluis@gmail.com>
2024-11-15 11:15:15 -0300

    prueba render 0.4

--- src/TourDataApp.py
+++ src/TourDataApp.py
@@ -12,21 +12,21 @@ from streamlit_folium import st_folium
 
 # Modelo y Diccionarios
 model = load(open('/opt/render/project/src/models/xgboost_regressor_lr_0_15_mx_dp_8_n_esti_310_42.pkl', 'rb'))
-with open('/Users/luiscamacho/VSCProjects/Exploracion_Proyecto_Final/data/processed/region_origen_dict.json', 'r', encoding='utf-8') as f:
+with open('/opt/render/project/src/data/processed/region_origen_dict.json', 'r', encoding='utf-8') as f:
     region_origen_dict = json.load(f)
-with open('/Users/luiscamacho/VSCProjects/Exploracion_Proyecto_Final/data/processed/region_destino_dict.json', 'r', encoding='utf-8') as f:
+with open('/opt/render/project/src/data/processed/region_destino_dict.json', 'r', encoding='utf-8') as f:
     region_destino_dict = json.load(f)
-with open('/Users/luiscamacho/VSCProjects/Exploracion_Proyecto_Final/data/processed/region_temp_dict.json', 'r', encoding='utf-8') as f:
+with open('/opt/render/project/src/data/processed/region_temp_dict.json', 'r', encoding='utf-8') as f:
     region_temp_dict = json.load(f)
-with open('/Users/luiscamacho/VSCProjects/Exploracion_Proyecto_Final/data/processed/region_pib_dict.json', 'r', encoding='utf-8') as f:
+with open('/opt/render/project/src/data/processed/region_pib_dict.json', 'r', encoding='utf-8') as f:
     region_pib_dict = json.load(f)
-with open('/Users/luiscamacho/VSCProjects/Exploracion_Proyecto_Final/data/processed/cut_comuna_dict.json', 'r', encoding='utf-8') as f:
+with open('/opt/render/project/src/data/processed/cut_comuna_dict.json', 'r', encoding='utf-8') as f:
     cut_comuna_dict = json.load(f)
-with open('/Users/luiscamacho/VSCProjects/Exploracion_Proyecto_Final/data/processed/cut_provincia_dict.json', 'r', encoding='utf-8') as f:
+with open('/opt/render/project/srcl/data/processed/cut_provincia_dict.json', 'r', encoding='utf-8') as f:
     cut_provincia_dict = json.load(f)
-with open('/Users/luiscamacho/VSCProjects/Exploracion_Proyecto_Final/data/processed/cut_region_dict.json', 'r', encoding='utf-8') as f:
+with open('/opt/render/project/src/data/processed/cut_region_dict.json', 'r', encoding='utf-8') as f:
     cut_region_dict = json.load(f)
-logo='/Users/luiscamacho/VSCProjects/Exploracion_Proyecto_Final/data/interim/TourData-remove-background.com.png'
+logo='/opt/render/project/src/data/interim/TourData-remove-background.com.png'
 
 # Funciones
 def obtener_info(region, region_dict, tipo='provincias', provincia=None): 
